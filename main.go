dcb8c43535aa9070978da9105a7b61dd0641848e
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-08-15 15:56:46 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+    var fileName string
+    if len(os.Args) > 1 {
+        fileName = os.Args[1]
+    } else {
+        fmt.Println("Please pass a file name")
+        os.Exit(1)
+    }
+    file, err := os.Open(fileName)
+    if err != nil {
+        fmt.Println("Err ", err)
+    }
+    scanner := bufio.NewScanner(file)
+    lines, words, characters := 0, 0, 0
+    for scanner.Scan() {
+        lines++
+
+        line := scanner.Text()
+        characters += len(line)
+
+        splitLines := strings.Split(line, " ")
+        words += len(splitLines)
+    }
+    fmt.Printf("%8d%8d%8d %s\n", lines, words, characters, fileName)
+}
