240bca08ca03ed232298ca9c22274dfc8ea5aa73
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-08-15 16:13:18 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,34 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
-	var fileName string
-	if len(os.Args) > 1 {
-		fileName = os.Args[1]
-	} else {
-		fmt.Println("Please pass a file name")
+	if len(os.Args) < 2 {
+		fmt.Println("Please pass at least one file name")
 		os.Exit(1)
 	}
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Err ", err)
-	}
-	scanner := bufio.NewScanner(file)
-	lines, words, characters := 0, 0, 0
-	for scanner.Scan() {
-		lines++
 
-		line := scanner.Text()
-		characters += len(line)
+	totalLines, totalWords, totalBytes := 0, 0, 0
 
-		splitLines := strings.Split(line, " ")
-		words += len(splitLines)
+	for _, fileName := range os.Args[1:] {
+		fileData, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Printf("Error reading %s: %s\n", fileName, err)
+			continue
+		}
+
+		content := string(fileData)
+		lines := strings.Split(content, "\n")
+		words := 0
+		bytes := len(fileData)
+
+		if len(lines) > 0 && len(lines[len(lines)-1]) == 0 {
+			lines = lines[:len(lines)-1]
+		}
+
+		for _, line := range lines {
+			words += len(strings.Fields(line))
+		}
+
+		fmt.Printf("%10d%10d%10d %s\n", len(lines), words, bytes, fileName)
+
+		totalLines += len(lines)
+		totalWords += words
+		totalBytes += bytes
 	}
-	fmt.Printf("%8d%8d%8d %s\n", lines, words, characters, fileName)
+
+	if len(os.Args) > 2 {
+		fmt.Printf("%10d%10d%10d total\n", totalLines, totalWords, totalBytes)
+	}
 }
